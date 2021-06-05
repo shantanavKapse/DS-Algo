5bf10e002aec69b6674b8a6b73d5ffc7013604d5
Shantanav Kapse <shantanavkapse73@gmail.com>
2021-06-05 21:13:16 +0530

    Added Buuble Sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,1 +1,14 @@
-print("Hello Everyone!")
+arr = [12, 34, 10, 6, 40]
+
+def bubbleSort(a):
+    n = len(a)
+    
+    for i in range(1, n):
+        for j in range(0, n-i):
+            if a[j] > a[j+1]:
+                tem = a[j]
+                a[j] = a[j+1]
+                a[j+1] = tem
+    return a
+
+print(bubbleSort(arr))
